e8d78ce177f7aa43847c53065b2acce2e11abecd
Blake <lakebayomackway@gmail.com>
2017-03-31 09:57:30 -0700

    add successful preDB tests to Client class.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,8 +2,33 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Client {
+  private String clientLastName;
+  private String clientFirstName;
+  private String clientLastVisit;
+  private String clientFirstVisit;
+  private int id;
 
-  public Client() {
 
+  public Client(String clientLastName, String clientFirstName, String clientLastVisit, String clientFirstVisit) {
+    this.clientLastName = clientLastName;
+    this.clientFirstName = clientFirstName;
+    this.clientLastVisit = clientLastVisit;
+    this.clientFirstVisit = clientFirstVisit;
+  }
+
+  public String getClientLastName() {
+    return clientLastName;
+  }
+
+  public String getClientFirstName() {
+    return clientFirstName;
+  }
+
+  public String getClientLastVisit() {
+    return clientLastVisit;
+  }
+
+  public String getClientFirstVisit() {
+    return clientFirstVisit;
   }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -3,9 +3,34 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ClientTest {
+
   @Test
   public void ClientInstantiatesCorrectly_true() {
-    Client myClient = new Client();
+    Client myClient = new Client("Simms", "Sally", "12/12/2016", "11/11/2005");
     assertEquals(true, myClient instanceof Client);
+  }
+
+  @Test
+  public void getClientLastName_returnsClientLastName_string() {
+    Client myClient = new Client("Simms", "Sally", "12/12/2016", "11/11/2005");
+    assertEquals("Simms", myClient.getClientLastName());
+  }
+
+  @Test
+  public void getClientFirsttName_returnsClientFirstName_string() {
+    Client myClient = new Client("Simms", "Sally", "12/12/2016", "11/11/2005");
+    assertEquals("Sally", myClient.getClientFirstName());
+  }
+
+  @Test
+  public void getClientLastVisit_returnsClientLastVisit_string() {
+    Client myClient = new Client("Simms", "Sally", "12/12/2016", "11/11/2005");
+    assertEquals("12/12/2016", myClient.getClientLastVisit());
+  }
+
+  @Test
+  public void getClientFirstVisit_returnsClientFirstVisit_string() {
+    Client myClient = new Client("Simms", "Sally", "12/12/2016", "11/11/2005");
+    assertEquals("11/11/2005", myClient.getClientFirstVisit());
   }
 }
