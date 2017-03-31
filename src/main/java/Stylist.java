09d6b0e561a4c25fad7e4452b9d0bb72d8744a31
Blake <lakebayomackway@gmail.com>
2017-03-31 09:37:39 -0700

    add successful preDB tests to Stylist class.

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -2,8 +2,26 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Stylist {
+  private String stylistLastName;
+  private String stylistFirstName;
+  private String stylistStatus;
+  private int id;
 
-  public Stylist() {
+  public Stylist(String stylistLastName, String stylistFirstName, String stylistStatus) {
+    this.stylistLastName = stylistLastName;
+    this.stylistFirstName = stylistFirstName;
+    this.stylistStatus = stylistStatus;
+  }
 
+  public String getStylistLastName() {
+    return stylistLastName;
+  }
+
+  public String getStylistFirstName() {
+    return stylistFirstName;
+  }
+
+  public String getStylistStatus() {
+    return stylistStatus;
   }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -5,8 +5,27 @@ import static org.junit.Assert.*;
 public class StylistTest {
   @Test
   public void StylistInstantiatesCorrectly_true() {
-    Stylist myStylist = new Stylist();
+    Stylist myStylist = new Stylist("Ritchie", "Danny", "FT");
     assertEquals(true, myStylist instanceof Stylist);
   }
+
+  @Test
+  public void getStylistLastName_returnsStylistLastName_string() {
+    Stylist myStylist = new Stylist("Ritchie", "Danny", "FT");
+    assertEquals("Ritchie", myStylist.getStylistLastName());
+  }
+
+  @Test
+  public void getStylistFirstName_returnsStylistFirstName_string() {
+    Stylist myStylist = new Stylist("Ritchie", "Danny", "FT");
+    assertEquals("Danny", myStylist.getStylistFirstName());
+  }
+
+  @Test
+  public void getStylistStatusreturnsStylistLastStatus_string() {
+    Stylist myStylist = new Stylist("Ritchie", "Danny", "FT");
+    assertEquals("FT", myStylist.getStylistStatus());
+  }
+
 
 }
